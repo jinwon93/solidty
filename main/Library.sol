5f658423e5a412c4e215a60c3beffba768fb2ec9
jinwon93 <kjw8208@nate.com>
2022-03-21 20:08:05 +0900

    20220321

--- main/Library.sol
+++ main/Library.sol
@@ -11,3 +11,35 @@ library SafeMath {
         return z;
     }
 }
+
+
+
+library Math {
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+        // else z = 0 (default value)
+    }
+}
+
+contract TestSafeMath {
+    using SafeMath for uint;
+
+    uint public MAX_UINT = 2**256 - 1;
+
+    function testAdd(uint x, uint y) public pure returns (uint) {
+        return x.add(y);
+    }
+
+    function testSquareRoot(uint x) public pure returns (uint) {
+        return Math.sqrt(x);
+    }
+}
