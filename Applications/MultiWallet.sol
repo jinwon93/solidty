c815d7837e6cba8ab1171cb5298612d4c3262f7d
jinwon93 <kjw8208@nate.com>
2022-03-27 19:13:39 +0900

    20220327

---
+++ Applications/MultiWallet.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.10;
+
+
+
+contract MultiWallet {
+
+    event Deposit (address indexed  sender , uint amount , uint balance);
+    event SubmitTransaction (
+
+        address indexed   owner , 
+        uint indexed  txIndex , 
+        address indexed  to, 
+        uint value , 
+        bytes data
+    );
+     event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    // mapping from tx index => owner => bool
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions; event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    // mapping from tx index => owner => bool
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+}
