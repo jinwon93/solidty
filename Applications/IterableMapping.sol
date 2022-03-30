636febed8c3a8ab32197a49c85f12c1ce1100ab9
jinwon93 <kjw8208@nate.com>
2022-03-30 19:39:53 +0900

    20220330

--- Applications/IterableMapping.sol
+++ Applications/IterableMapping.sol
@@ -11,4 +11,17 @@ library IterableMapping {
         mapping(address => uint) indexOf;
         mapping(address => bool) inserted;
     }
+
+
+     function get(Map storage map, address key) public view returns (uint) {
+        return map.values[key];
+    }
+
+    function getKeyAtIndex(Map storage map, uint index) public view returns (address) {
+        return map.keys[index];
+    }
+
+    function size(Map storage map) public view returns (uint) {
+        return map.keys.length;
+    }
 }
