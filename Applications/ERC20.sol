df03b41557dfe597c6913ead768998e2bfd93f35
jinwon93 <kjw8208@nate.com>
2022-04-01 20:15:05 +0900

    20220401

---
+++ Applications/ERC20.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.10;
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+}
