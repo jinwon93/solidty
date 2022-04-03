ba5e6201c3c06d68e154dabfaf99b5f83f5316d3
jinwon93 <kjw8208@nate.com>
2022-04-03 18:54:32 +0900

    20220403

---
+++ Applications/ERC721.sol
@@ -0,0 +1,24 @@
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+interface IERC721 is IERC165 {
+    function balanceOf(address owner) external view returns (uint balance);
+
+    function ownerOf(uint tokenId) external view returns (address owner);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId,
+        bytes calldata data
+    ) external;
+
+    
+}
