c5e1cfba39926cd1a106b29980af3c37a3b20bda
jinwon93 <kjw8208@nate.com>
2022-04-20 20:31:56 +0900

    20220420

--- Applications/ContractAddress.sol
+++ Applications/ContractAddress.sol
@@ -44,4 +44,39 @@ contract FactoryAssembly {
             // NOTE: cast last 20 bytes of hash to address
             return address(uint160(uint(hash)));
         }
+
+
+         // 3. Deploy the contract
+    // NOTE:
+    // Check the event log Deployed which contains the address of the deployed TestContract.
+    // The address in the log should equal the address computed from above.
+    function deploy(bytes memory bytecode, uint _salt) public payable {
+        address addr;
+
+        /*
+        NOTE: How to call create2
+
+        create2(v, p, n, s)
+        create new contract with code at memory p to p + n
+        and send v wei
+        and return the new address
+        where new address = first 20 bytes of keccak256(0xff + address(this) + s + keccak256(mem[p…(p+n)))
+              s = big-endian 256-bit value
+        */
+        assembly {
+            addr := create2(
+                callvalue(), // wei sent with current call
+                // Actual code starts after skipping the first 32 bytes
+                add(bytecode, 0x20),
+                mload(bytecode), // Load the size of code contained in the first 32 bytes
+                _salt // Salt from function arguments
+            )
+
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+
+        emit Deployed(addr, _salt);
+        }
     }
