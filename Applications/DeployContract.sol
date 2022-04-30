a247632a21c9149d20491e67f65bfecc58e8240d
jinwon93 <kjw8208@nate.com>
2022-04-30 20:09:05 +0900

    20220430

--- Applications/DeployContract.sol
+++ Applications/DeployContract.sol
@@ -26,4 +26,27 @@ contract Proxy {
         (bool success, ) = _target.call{value: msg.value}(_data);
         require(success, "failed");
     }
+
+
+    contract TestContract1 {
+    address public owner = msg.sender;
+
+    function setOwner(address _owner) public {
+        require(msg.sender == owner, "not owner");
+        owner = _owner;
+    }
+}
+
+contract TestContract2 {
+    address public owner = msg.sender;
+    uint public value = msg.value;
+    uint public x;
+    uint public y;
+
+    constructor(uint _x, uint _y) payable {
+        x = _x;
+        y = _y;
+    }
+}
+
 }
