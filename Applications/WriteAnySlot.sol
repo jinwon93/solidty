e03a3436bb3f15060cee8178f4cfaf09bf6ae56c
jinwon93 <kjw8208@nate.com>
2022-05-03 20:35:44 +0900

    20220503

--- Applications/WriteAnySlot.sol
+++ Applications/WriteAnySlot.sol
@@ -21,4 +21,24 @@ contract Storage {
             s.slot := i
         }
     }
+
+        /*
+    get(0) returns 123
+    get(1) returns 456
+    get(2) returns 789
+    */
+    function get(uint i) external view returns (uint) {
+        // get value inside MyStruct stored at slot i
+        return _get(i).value;
+    }
+
+    /*
+    We can save data to any slot including slot 999 which is normally unaccessble.
+
+    set(999) = 888 
+    */
+    function set(uint i, uint x) external {
+        // set value of MyStruct to x and store it at slot i
+        _get(i).value = x;
+    }
 }    
