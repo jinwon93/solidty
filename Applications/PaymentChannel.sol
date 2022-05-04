34e85bc5a1041cd7ceef24f5cf7813cf51b6c4f5
jinwon93 <kjw8208@nate.com>
2022-05-04 23:06:11 +0900

    20220504

---
+++ Applications/PaymentChannel.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.10;
+
+
+
+
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/security/ReentrancyGuard.sol";
+
+contract UniDirectionalPaymentChannel is ReentrancyGuard {
+    using ECDSA for bytes32;
+
+    address payable public sender;
+    address payable public receiver;
+
+    uint private constant DURATION = 7 * 24 * 60 * 60;
+    uint public expiresAt;
+
+    constructor(address payable _receiver) payable {
+        require(_receiver != address(0), "receiver = zero address");
+        sender = payable(msg.sender);
+        receiver = _receiver;
+        expiresAt = block.timestamp + DURATION;
+    }
+}
