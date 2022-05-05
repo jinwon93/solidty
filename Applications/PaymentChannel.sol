668633eea8e1ce6f8398048f99a9cb2bc3941af1
jinwon93 <kjw8208@nate.com>
2022-05-05 16:08:05 +0900

    20220505

--- Applications/PaymentChannel.sol
+++ Applications/PaymentChannel.sol
@@ -21,4 +21,24 @@ contract UniDirectionalPaymentChannel is ReentrancyGuard {
         receiver = _receiver;
         expiresAt = block.timestamp + DURATION;
     }
+
+
+    function _getHash(uint _amount) private view returns (bytes32) {
+        // NOTE: sign with address of this contract to protect agains
+        // replay attack on other contracts
+        return keccak256(abi.encodePacked(address(this), _amount));
+    }
+
+    function getHash(uint _amount) external view returns (bytes32) {
+        return _getHash(_amount);
+    }
+
+    function _getEthSignedHash(uint _amount) private view returns (bytes32) {
+        return _getHash(_amount).toEthSignedMessageHash();
+    }
+
+    function getEthSignedHash(uint _amount) external view returns (bytes32) {
+        return _getEthSignedHash(_amount);
+    }
+
 }
