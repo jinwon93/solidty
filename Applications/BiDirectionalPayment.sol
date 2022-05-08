7a060fbd7f2a3b79ef2df63008575e56792378c7
jinwon93 <kjw8208@nate.com>
2022-05-08 19:43:40 +0900

    20220508

--- Applications/BiDirectionalPayment.sol
+++ Applications/BiDirectionalPayment.sol
@@ -21,4 +21,36 @@ contract BiDirectionalPaymentChannel {
     uint public challengePeriod;
     uint public expiresAt;
     uint public nonce;
+
+     modifier checkBalances(uint[2] memory _balances) {
+        require(
+            address(this).balance >= _balances[0].add(_balances[1]),
+            "balance of contract must be >= to the total balance of users"
+        );
+        _;
+    }
+
+    // NOTE: deposit from multi-sig wallet
+    constructor(
+        address payable[2] memory _users,
+        uint[2] memory _balances,
+        uint _expiresAt,
+        uint _challengePeriod
+    ) payable checkBalances(_balances) {
+        require(_expiresAt > block.timestamp, "Expiration must be > now");
+        require(_challengePeriod > 0, "Challenge period must be > 0");
+
+        for (uint i = 0; i < _users.length; i++) {
+            address payable user = _users[i];
+
+            require(!isUser[user], "user must be unique");
+            users[i] = user;
+            isUser[user] = true;
+
+            balances[user] = _balances[i];
+        }
+
+        expiresAt = _expiresAt;
+        challengePeriod = _challengePeriod;
+    }
 }    
