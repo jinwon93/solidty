20e0b7f0ca10e04e268432e7808cb647f7b84cbc
jinwon93 <kjw8208@nate.com>
2022-05-11 21:58:16 +0900

    20220511

---
+++ Applications/EnglishAuction.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.10;
+
+
+
+
+interface IERC721 {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external;
+}
