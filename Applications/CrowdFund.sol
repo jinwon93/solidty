684d69e00066de8d4d3480e89aeee18736225bc1
jinwon93 <kjw8208@nate.com>
2022-05-18 23:09:12 +0900

    20220518

---
+++ Applications/CrowdFund.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.13;
+
+
+
+
+
+
+interface IERC20 {
+    function transfer(address, uint) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external returns (bool);
+}
+
+
+contract CrowdFund {
+    event Launch(
+        uint id,
+        address indexed creator,
+        uint goal,
+        uint32 startAt,
+        uint32 endAt
+    );
+    event Cancel(uint id);
+    event Pledge(uint indexed id, address indexed caller, uint amount);
+    event Unpledge(uint indexed id, address indexed caller, uint amount);
+    event Claim(uint id);
+    event Refund(uint id, address indexed caller, uint amount);
+}
