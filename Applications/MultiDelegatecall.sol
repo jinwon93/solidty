3c8e61bc90024c5c4fe0baf27448d8a66531fa86
jinwon93 <kjw8208@nate.com>
2022-05-28 18:55:18 +0900

    20220528

---
+++ Applications/MultiDelegatecall.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.13;
+
+contract MultiDelegatecall {
+    error DelegatecallFailed();
+
+    function multiDelegatecall(bytes[] memory data)
+        external
+        payable
+        returns (bytes[] memory results)
+    {
+        results = new bytes[](data.length);
+
+        for (uint i; i < data.length; i++) {
+            (bool ok, bytes memory res) = address(this).delegatecall(data[i]);
+            if (!ok) {
+                revert DelegatecallFailed();
+            }
+            results[i] = res;
+        }
+    }
+}
+
