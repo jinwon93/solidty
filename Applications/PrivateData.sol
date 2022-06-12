c8e6d83e15b8dab930d7b8b485d88e681367cced
jinwon93 <kjw8208@nate.com>
2022-06-12 20:06:30 +0900

    20220612

---
+++ Applications/PrivateData.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.13;
+
+
+
+
+contract Vault {
+    // slot 0
+    uint public count = 123;
+    // slot 1
+    address public owner = msg.sender;
+    bool public isTrue = true;
+    uint16 public u16 = 31;
+    // slot 2
+    bytes32 private password;
+
+    // constants do not use storage
+    uint public constant someConst = 123;
+
+    // slot 3, 4, 5 (one for each array element)
+    bytes32[3] public data;
+
+    struct User {
+        uint id;
+        bytes32 password;
+    }
+
+}    
