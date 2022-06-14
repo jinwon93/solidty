a549e48570b3879ecf5d0bb9fd93dbe45c54b072
jinwon93 <kjw8208@nate.com>
2022-06-14 22:14:33 +0900

    20220614

---
+++ Applications/Delegatecall.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.13;
+
+
+
+contract Lib {
+    address public owner;
+
+    function pwn() public {
+        owner = msg.sender;
+    }
+}
+
+contract HackMe {
+    address public owner;
+    Lib public lib;
+
+    constructor(Lib _lib) {
+        owner = msg.sender;
+        lib = Lib(_lib);
+    }
+
+    fallback() external payable {
+        address(lib).delegatecall(msg.data);
+    }
+}
