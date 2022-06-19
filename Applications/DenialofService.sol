84746d82f243de6d8f107351211ad3db8c8ec9f2
jinwon93 <kjw8208@nate.com>
2022-06-19 15:13:59 +0900

    20220619

---
+++ Applications/DenialofService.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.13;
+
+
+
+contract KingOfEther {
+    address public king;
+    uint public balance;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "Need to pay more to become the king");
+
+        (bool sent, ) = king.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+}
