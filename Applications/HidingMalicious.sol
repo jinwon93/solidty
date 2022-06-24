4484e32b6ab6491323fd124c0047032dd1e03e03
jinwon93 <kjw8208@nate.com>
2022-06-24 21:18:22 +0900

    20220624

---
+++ Applications/HidingMalicious.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.13;
+
+
+
+
+contract Foo {
+    Bar bar;
+
+    constructor(address _bar) {
+        bar = Bar(_bar);
+    }
+
+    function callBar() public {
+        bar.log();
+    }
+}
+
+contract Bar {
+    event Log(string message);
+
+    function log() public {
+        emit Log("Bar was called");
+    }
+}
