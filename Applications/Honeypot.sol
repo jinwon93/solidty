38da7ba092cb720fddbc936c5ee29973f9fb8894
jinwon93 <kjw8208@nate.com>
2022-06-26 20:46:18 +0900

    20220626

---
+++ Applications/Honeypot.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.13;
+
+
+
+contract Bank {
+    mapping(address => uint) public balances;
+    Logger logger;
+
+    constructor(Logger _logger) {
+        logger = Logger(_logger);
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        logger.log(msg.sender, msg.value, "Deposit");
+    }
+
+    function withdraw(uint _amount) public {
+        require(_amount <= balances[msg.sender], "Insufficient funds");
+
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] -= _amount;
+
+        logger.log(msg.sender, _amount, "Withdraw");
+    }
+}
