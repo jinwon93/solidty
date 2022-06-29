f48db33198a599bedf8f8109b6d63a4dba1691c3
jinwon93 <kjw8208@nate.com>
2022-06-29 22:02:19 +0900

    20220629

--- Applications/Honeypot.sol
+++ Applications/Honeypot.sol
@@ -61,4 +61,22 @@ contract Attack {
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
+
+    
+contract HoneyPot {
+    function log(
+        address _caller,
+        uint _amount,
+        string memory _action
+    ) public {
+        if (equal(_action, "Withdraw")) {
+            revert("It's a trap");
+        }
+    }
+
+    // Function to compare strings using keccak256
+    function equal(string memory _a, string memory _b) public pure returns (bool) {
+        return keccak256(abi.encode(_a)) == keccak256(abi.encode(_b));
+    }
+}
 }
