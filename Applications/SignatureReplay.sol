2b366966364489d606d91d68ad6430bc22ad7846
jinwon93 <kjw8208@nate.com>
2022-07-05 22:14:00 +0900

    20220705

---
+++ Applications/SignatureReplay.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.13;
+pragma experimental ABIEncoderV2;
+
+
+
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol";
+
+contract MultiSigWallet {
+    using ECDSA for bytes32;
+
+    address[2] public owners;
+
+    constructor(address[2] memory _owners) payable {
+        owners = _owners;
+    }
+
+    function deposit() external payable {}
+
+    function transfer(
+        address _to,
+        uint _amount,
+        bytes[2] memory _sigs
+    ) external {
+        bytes32 txHash = getTxHash(_to, _amount);
+        require(_checkSigs(_sigs, txHash), "invalid sig");
+
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+}    
