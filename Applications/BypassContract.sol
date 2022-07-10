2ce6ceb284ecb3e37524ef1ed883cb0bc448c954
jinwon93 <59594415+jinwon93@users.noreply.github.com>
2022-07-10 21:57:13 +0900

    20200710
    20200710

--- Applications/BypassContract.sol
+++ Applications/BypassContract.sol
@@ -21,3 +21,11 @@ contract Target {
 }
 
 
+contract FailedAttack {
+    // Attempting to call Target.protected will fail,
+    // Target block calls from contract
+    function pwn(address _target) external {
+        // This will fail
+        Target(_target).protected();
+    }
+}
