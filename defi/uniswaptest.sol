ecbb17e858ef9ba22892809b70dc02aa123a6d0d
jinwon93 <kjw8208@nate.com>
2022-07-17 16:11:54 +0900

    20220717

--- defi/uniswaptest.sol
+++ defi/uniswaptest.sol
@@ -48,3 +48,22 @@ interface IUniswapV2Router {
         uint deadline
     ) external returns (uint[] memory amounts);
 }
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+}
