57e11951093224aeb4ade50b4809063cbc45c144
jinwon93 <kjw8208@nate.com>
2022-07-18 21:28:13 +0900

    20220718

---
+++ defi/RemoveLiquidity.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.13;
+
+contract TestUniswapLiquidity {
+    address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function addLiquidity(
+        address _tokenA,
+        address _tokenB,
+        uint _amountA,
+        uint _amountB
+    ) external {
+        IERC20(_tokenA).transferFrom(msg.sender, address(this), _amountA);
+        IERC20(_tokenB).transferFrom(msg.sender, address(this), _amountB);
+
+        IERC20(_tokenA).approve(ROUTER, _amountA);
+        IERC20(_tokenB).approve(ROUTER, _amountB);
+    }        
