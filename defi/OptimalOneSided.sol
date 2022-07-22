3c882dfe0bd600431702b3673f8b7307e41725e2
jinwon93 <kjw8208@nate.com>
2022-07-22 13:53:35 +0900

    20220722

---
+++ defi/OptimalOneSided.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.13;
+
+contract TestUniswapOptimalOneSidedSupply {
+    address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function sqrt(uint y) private pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+}
