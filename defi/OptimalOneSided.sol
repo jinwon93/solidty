5c86d1719fcd29fa56e7e6fb07db14ac7e061ab5
jinwon93 <kjw8208@nate.com>
2022-07-26 22:03:02 +0900

    20220726

--- defi/OptimalOneSided.sol
+++ defi/OptimalOneSided.sol
@@ -73,3 +73,30 @@ contract TestUniswapOptimalOneSidedSupply {
             block.timestamp
         );
     }
+
+    interface IUniswapV2Router {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    )
+        external
+        returns (
+            uint amountA,
+            uint amountB,
+            uint liquidity
+        );
+
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
