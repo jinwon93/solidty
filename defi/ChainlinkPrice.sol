60f05cb5411224b4ae2341445db7c68a1dbd159f
jinwon93 <kjw8208@nate.com>
2022-07-29 18:55:02 +0900

    20220729

---
+++ defi/ChainlinkPrice.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.13;
+
+
+
+contract ChainlinkPriceOracle {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        // ETH / USD
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        // for ETH / USD price is scaled up by 10 ** 8
+        return price / 1e8;
+    }
+}
