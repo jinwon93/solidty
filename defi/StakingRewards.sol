ed2cc868d52417ed480b93f4a07be8f4fa2977f4
jinwon93 <kjw8208@nate.com>
2022-07-31 21:30:38 +0900

    20220731

---
+++ defi/StakingRewards.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8;
+
+contract StakingRewards {
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardsToken;
+
+    address public owner;
+
+    // Duration of rewards to be paid out (in seconds)
+    uint public duration;
+    // Timestamp of when the rewards finish
+    uint public finishAt;
+    // Minimum of last updated time and reward finish time
+    uint public updatedAt;
+    // Reward to be paid out per second
+    uint public rewardRate;
+
+}    
