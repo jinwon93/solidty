0afcc5faa4944ebbfaa6e3d95203807b06b39a41
jinwon93 <kjw8208@nate.com>
2022-08-07 20:51:47 +0900

    20220807

--- defi/StakingRewards.sol
+++ defi/StakingRewards.sol
@@ -100,6 +100,33 @@ contract StakingRewards {
         require(finishAt < block.timestamp, "reward duration not finished");
         duration = _duration;
     }
+
+
+    function notifyRewardAmount(uint _amount)
+        external
+        onlyOwner
+        updateReward(address(0))
+    {
+        if (block.timestamp >= finishAt) {
+            rewardRate = _amount / duration;
+        } else {
+            uint remainingRewards = (finishAt - block.timestamp) * rewardRate;
+            rewardRate = (_amount + remainingRewards) / duration;
+        }
+
+        require(rewardRate > 0, "reward rate = 0");
+        require(
+            rewardRate * duration <= rewardsToken.balanceOf(address(this)),
+            "reward amount > balance"
+        );
+
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+    }
+
+    function _min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
+    }
 }    
 
 
