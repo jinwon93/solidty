8eec5b796486a12c416cc5cb798c7fc173a25263
jinwon93 <kjw8208@nate.com>
2022-08-16 13:36:05 +0900

    20220816

---
+++ defi/AMMsum.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.13;
+
+contract CSAMM {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+    uint public reserve0;
+    uint public reserve1;
+
+}
+
+interface IERC20 {
+
+    
+}
